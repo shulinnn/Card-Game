f1f42a5334f8fb38bca76bb5f60ba0cf2d53012b
shulinnn <shulin.git@gmail.com>
2020-11-25 13:53:30 +0100

    Add New Unity Input System

---
+++ Assets/Scripts/Multiplayer/InputComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Mirror;
+
+namespace Assets.Scripts.Multiplayer
+{
+    public class InputComponent : NetworkBehaviour
+    {
+
+        public static System.Action<NetworkIdentity, int> OnCardUsage;
+
+        [SerializeField]
+        private KeyCode[] KeyCodes;
+
+        private void Update()
+        {
+            if (!isLocalPlayer)
+                return;
+        }
+
+    }
+}
