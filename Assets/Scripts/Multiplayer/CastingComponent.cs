6e5615dc0fafc0265fdc76dd592ce1907e195333
shulinnn <shulin.git@gmail.com>
2020-11-26 21:22:32 +0100

    Update player

--- Assets/Scripts/Multiplayer/CastingComponent.cs
+++ Assets/Scripts/Multiplayer/CastingComponent.cs
@@ -6,5 +6,20 @@ namespace Assets.Scripts.Multiplayer
     public class CastingComponent : NetworkBehaviour
     {
 
+        private InputComponent inputComponent;
+
+        private void Start()
+        {
+            inputComponent = GetComponent<InputComponent>();
+
+            inputComponent.OnCardUsageAction += OnCardUsage;
+
+        }
+
+        void OnCardUsage(int i)
+        {
+            Debug.Log("CardUsage" + i);
+        }
+
     }
 }
--- Assets/Scripts/Multiplayer/InputComponent.cs
+++ Assets/Scripts/Multiplayer/InputComponent.cs
@@ -6,5 +6,24 @@ namespace Assets.Scripts.Multiplayer
 
     public class InputComponent : NetworkBehaviour
     {
+
+        public System.Action<int> OnCardUsageAction;
+
+        [SerializeField]
+        private KeyCode[] cardKeyCodes;
+
+        private void Update()
+        {
+
+            if (!isLocalPlayer)
+                return;
+
+            for (int i = 0; i < cardKeyCodes.Length; i++)
+            {
+                if(Input.GetKeyDown(cardKeyCodes[i]))
+                    OnCardUsageAction?.Invoke(i);
+            }
+        }
+
     }
 }
