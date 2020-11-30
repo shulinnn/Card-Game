1071d72e77c1d6aefc7b9d2a89198505b802f628
shulinnn <shulin.git@gmail.com>
2020-11-30 14:34:45 +0100

    Create Spawning component

---
+++ Assets/Scripts/Multiplayer/SpawningComponent.cs
@@ -0,0 +1,50 @@
+﻿using Assets.Scripts.Core;
+using Mirror;
+using UnityEngine;
+
+namespace Assets.Scripts.Multiplayer
+{
+    public class SpawningComponent : NetworkBehaviour
+    {
+        public System.Action<Card, Vector3> OnPointTargetingSuccessAction;
+        public System.Action<Card, GameObject> OnMinionTargetingSuccessAction;
+        public System.Action<Card, GameObject> OnSummonerTargetingSuccessAction;
+
+        private void Awake()
+        {
+            OnPointTargetingSuccessAction += SpawnComponentPoint;
+        }
+
+        [Client]
+        public void SpawnComponentPoint(Card card, Vector3 pointObject) => CmdSpellOnPoint(card, pointObject);
+
+        [Client]
+        public void SpawnComponentMinion(Card card, GameObject minionObject) { }
+
+        [Client]
+        public void SpawnComponentSummoner(Card card, GameObject summonerObject) { }
+
+
+        [Command]
+        void CmdSpellOnPoint(Card card, Vector3 point)
+        {
+            if (card.traversalType == TraversalType.FromCasterToPos)
+            {
+                //spawn at self.
+            }
+            if (card.traversalType == TraversalType.SpawnAtPoint)
+            {
+                //spawn at point
+            }
+
+        }
+        [Command]
+        void CmdSpellOnMinion(Card card, GameObject minionObject) { }
+        [Command]
+        void CmdSpellOnSummoner(Card card, GameObject summonerObject) { }
+        [Command]
+        void CmdEnchantmentOnMinion(Card card, GameObject minionObject) { }
+        [Command]
+        void CmdMinionSpawn(Card card) { }
+    }
+}
--- Assets/Scripts/Multiplayer/TargetingComponent.cs
+++ Assets/Scripts/Multiplayer/TargetingComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scripts.Core;
 using Mirror;
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Scripts.Multiplayer
@@ -7,7 +8,27 @@ namespace Assets.Scripts.Multiplayer
     public class TargetingComponent : NetworkBehaviour
     {
 
-        [SerializeField] private bool isTargeting;
+        #region Properties
+        [SerializeField] protected bool isTargeting { get; set; }
+
+        [SerializeField] protected LayerMask groundMask, minionMask, summonerMask;
+
+        protected SpawningComponent spawningComponent;
+
+        #endregion
+
+        private void Start()
+        {
+            spawningComponent = GetComponent<SpawningComponent>();
+        }
+
+        void InitTargeting(Card card)
+        {
+            if (isTargeting)
+                return;
+
+            DetectTarget(card);
+        }
 
         /// <summary>
         /// Here we initialize Client side part of targeting
@@ -15,10 +36,137 @@ namespace Assets.Scripts.Multiplayer
         /// <param name="card">The card we're using</param>
         /// 
         [Client]
-        public void Initialize(Card card)
-        {
+        public void Initialize(Card card) => InitTargeting(card);
 
+        [Client]
+        void DetectTarget(Card card)
+        {
+            isTargeting = true;
+
+            switch (card.cardType)
+            {
+                case CardType.Spell:
+                    {
+                        switch (card.targetType)
+                        {
+                            case TargetType.Minion:
+                                {
+                                    StartCoroutine(WaitForMinion(card));
+                                }
+                                break;
+                            case TargetType.Summoner:
+                                {
+                                    StartCoroutine(WaitForSummoner(card));
+                                }
+                                break;
+                            case TargetType.Point:
+                                {
+                                    StartCoroutine(WaitForPoint(card));
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    break;
+                case CardType.Enchantment:
+                    {
+                        ///
+                    }
+                    break;
+                case CardType.Summon:
+                    {
+                        ///
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
+
+        #region Coroutines
+        IEnumerator WaitForMinion(Card card)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, minionMask))
+                {
+
+                    /// We have successfully found an target => invoke spawning thingy.
+                    /// 
+                    isTargeting = false;
+                    yield break;
+                }
+                else
+                {
+                    isTargeting = false;
+                    yield break;
+                }
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                isTargeting = false;
+                yield break;
+            }
+            yield return null;
+        }
+
+        IEnumerator WaitForSummoner(Card card)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, summonerMask))
+                {
+                    isTargeting = false;
+                    yield break;
+                }
+                else
+                {
+                    isTargeting = false;
+                    yield break;
+                }
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                isTargeting = false;
+                yield break;
+            }
+            yield return null;
+        }
+
+        IEnumerator WaitForPoint(Card card)
+        {
+            while (isTargeting)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit hit = new RaycastHit();
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, groundMask))
+                    {
+
+                        spawningComponent.OnPointTargetingSuccessAction?.Invoke(card, hit.point);
+
+                        isTargeting = false;
+                        yield break;
+                    }
+                    else
+                    {
+                        isTargeting = false;
+                        yield break;
+                    }
+                }
+                else if (Input.GetMouseButtonDown(1))
+                {
+                    isTargeting = false;
+                    yield break;
+                }
+                yield return null;
+            }
+        }
+
+        #endregion
 
     }
 }
