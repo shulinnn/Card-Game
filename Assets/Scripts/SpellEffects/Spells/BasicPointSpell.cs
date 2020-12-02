17897399214f2a0d2406a6a1abba0b7d72e6d764
shulinnn <shulin.git@gmail.com>
2020-12-02 04:34:34 +0100

    update point spell

---
+++ Assets/Scripts/SpellEffects/Spells/BasicPointSpell.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.SpellEffects.Bases;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.SpellEffects.Spells
+{
+
+
+    public class BasicPointSpell : ProjectileSpellPoint
+    {
+        public override void OnProjectileHit(GameObject gObject)
+        {
+
+            base.OnProjectileHit(gObject);
+
+            Debug.Log(gObject.name);
+        }
+    }
+}
