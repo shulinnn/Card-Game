5634dd893404ed41bc1503c30b04f7694d326d7b
shulinnn <shulin.git@gmail.com>
2020-12-07 11:51:47 +0100

    add spell target base

---
+++ Assets/Scripts/SpellEffects/Bases/SpellTarget.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.SpellEffects.Bases
+{
+    public class SpellTarget : SpellEffectBase
+    {
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+
+            OnSpellSpawn();
+
+            StartCoroutine(OnSpellTick());
+
+            StartCoroutine(SpellEffectTimer());
+        }
+
+        internal IEnumerator SpellEffectTimer()
+        {
+            yield return new WaitForSeconds(_card.spellEffect.lifetime);
+            OnSpellEnd();
+        }
+
+        public virtual void OnSpellSpawn() { }
+
+        /// <summary>
+        /// Called each second after spawning
+        /// </summary>
+        public IEnumerator OnSpellTick() 
+        {
+
+            Tick();
+
+            yield return new WaitForSeconds(1f);
+        }
+
+        public virtual void Tick() { }
+
+        public virtual void OnSpellEnd() { }
+
+    }
+}
