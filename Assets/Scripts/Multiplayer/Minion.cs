7f41760594d02ab5946c0482a1ca067ba18e06e8
shulinnn <shulin.git@gmail.com>
2020-12-07 12:59:57 +0100

    Update minion

--- Assets/Scripts/Multiplayer/Minion.cs
+++ Assets/Scripts/Multiplayer/Minion.cs
@@ -14,9 +14,43 @@ namespace Assets.Scripts.Multiplayer
         public float moveSpeed;
     }
 
+    [Serializable]
+    public enum MinionState
+    {
+      Move,
+      Attack,
+      Chase
+    }
+
     public class Minion : NetworkBehaviour
     {
-        public MinionObject minionObject;
+        [SerializeField]
+        private MinionObject minionObject;
+        [SerializeField]
+        private MinionState minionState;
+        [SerializeField]
+        private HealthComponent healthComponent;
+
+        [SerializeField]
+        private Transform[] wayPoints;
+
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+
+
+
+        }
+
+        [Server]
+        public void Update()
+        {
+            
+        }
+
+
 
     }
+
 }
