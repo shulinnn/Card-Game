e8061a5f06fa2fed942d2f5f9aa84b40948170de
shulinnn <shulin.git@gmail.com>
2020-12-07 11:51:28 +0100

    update & create spells

---
+++ Assets/Scripts/SpellEffects/Spells/BasicEnchantment.cs
@@ -0,0 +1,25 @@
+﻿using Mirror;
+using UnityEngine;
+using Assets.Scripts.SpellEffects.Bases;
+using Assets.Scripts.Multiplayer;
+
+namespace Assets.Scripts.SpellEffects.Spells
+{
+    public class BasicEnchantment : EnchantmentBase
+    {
+        public override void OnEnchantmentStart()
+        {
+            base.OnEnchantmentStart();
+            _targetTransform.gameObject.GetComponent<HealthComponent>().maxHealth += _card.cardValue;
+        }
+
+        public override void OnEnchantmentEnd()
+        {
+            base.OnEnchantmentEnd();
+
+            _targetTransform.gameObject.GetComponent<HealthComponent>().maxHealth -= _card.cardValue;
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/SpellEffects/Spells/BasicSummonerSpell.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.SpellEffects.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.SpellEffects.Spells
+{
+    public class BasicSummonerSpell : SpellTarget
+    {
+
+        public override void OnSpellSpawn()
+        {
+            base.OnSpellSpawn();
+        }
+
+
+        public override void OnSpellEnd()
+        {
+            base.OnSpellEnd();
+
+            Destroy(gameObject);
+
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+        }
+
+    }
+}
