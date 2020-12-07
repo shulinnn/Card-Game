8cdbff2347bb057cf650923f991710ad622b8fd2
shulinnn <shulin.git@gmail.com>
2020-12-07 11:50:55 +0100

    add enchantment base

---
+++ Assets/Scripts/SpellEffects/Bases/EnchantmentBase.cs
@@ -0,0 +1,37 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.SpellEffects.Bases
+{
+    public class EnchantmentBase : SpellEffectBase
+    {
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+
+            OnEnchantmentStart();
+
+            StartCoroutine(OnEnchantmentTick());
+
+        }
+        [Server]
+        public virtual void OnEnchantmentStart() { }
+
+        [Server]
+        public virtual void OnEnchantmentEnd() { }
+        internal IEnumerator OnEnchantmentTick()
+        {
+            Tick();
+            yield return new WaitForSeconds(1f);
+        }
+
+        /// <summary>
+        /// Gets called each second spell effect is enabled/alive
+        /// </summary>
+        public virtual void Tick() { }
+
+    }
+}
