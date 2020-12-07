2583a8a99c38a692ab267fd9a0e2933d20579c59
shulinnn <shulin.git@gmail.com>
2020-12-07 11:47:44 +0100

    create minion component

---
+++ Assets/Scripts/Multiplayer/Minion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Mirror;
+
+namespace Assets.Scripts.Multiplayer
+{
+    [Serializable]
+    public struct MinionObject
+    {
+        public float attackDamage;
+        public float attackSpeed;
+        public float attackRange;
+        public float moveSpeed;
+    }
+
+    public class Minion : NetworkBehaviour
+    {
+        public MinionObject minionObject;
+
+    }
+}
