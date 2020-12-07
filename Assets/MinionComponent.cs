b281aea34717eeac3df4c98138ab32f8e93b1471
shulinnn <shulin.git@gmail.com>
2020-12-07 11:50:06 +0100

    add minion component

---
+++ Assets/MinionComponent.cs
@@ -0,0 +1,15 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+
+    public class MinionComponent : NetworkBehaviour
+    {
+
+        public Transform bottomTransform;
+    }
+
+}
